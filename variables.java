e2200fe7ad9723c229ba4c86a4153b9c692d7979
Amruta <amrutabmaskar@gmail.com>
2025-01-29 14:34:01 +0530

    Use variables

--- variables.java
+++ variables.java
@@ -1,5 +1,19 @@
 public class variables {
     public static void main(String[] args) {
+        int $num1=10; //variable initialization
+        int num=20;
+        int FOR=10;
+        long i=215874855;
+        String name="Amruta";
+        System.out.println($num1);
+        System.out.println(num);
+        System.out.println(FOR);
+        System.out.println(i);
+        System.out.println(name);
+
+        String name1; //variable declaration
+        name1="Coco";
+        System.out.println(name1);
 
     }
 }
