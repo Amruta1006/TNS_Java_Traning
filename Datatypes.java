b1a204dafb5cc60c3a57b806f4e989c59ee3ddb9
Amruta <amrutabmaskar@gmail.com>
2025-01-29 14:42:06 +0530

    Use datatypes

---
+++ Datatypes.java
@@ -0,0 +1,18 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int value = 9/2;
+        System.out.println(value);
+
+        float value1 = 10f/06f;
+        System.out.println(value1);
+
+        double value2 = 10d/06d;
+        System.out.println(value2);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        char ch1 = 65;
+        System.out.println(ch1);
+    }
+}
