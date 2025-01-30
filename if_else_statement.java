58367cc189278e14d71a27a854f4c877ed7c58e1
Amruta <amrutabmaskar@gmail.com>
2025-01-30 16:03:07 +0530

    if-else statement

---
+++ if_else_statement.java
@@ -0,0 +1,12 @@
+public class if_else_statement {
+    public static void main(String[] args) {
+        int age = 21;
+        if(age>18) {
+            System.out.println("Eligible for exam");
+        }
+            else{
+                System.out.println("Not eligible");
+            }
+        }
+    }
+
