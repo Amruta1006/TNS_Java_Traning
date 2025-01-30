3f9bba79e6406a0f0c95165f28b032f60f2bb6b8
Amruta <amrutabmaskar@gmail.com>
2025-01-30 16:00:00 +0530

    if statement

---
+++ ifStatement.java
@@ -0,0 +1,9 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        int x=9;
+        if(x==5)
+        {
+            System.out.println("value of x is 5");
+        }
+    }
+}
