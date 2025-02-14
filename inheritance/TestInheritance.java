ddc4ef69e46d7b72380a8c338b519d58fc94179e
Amruta <amrutabmaskar@gmail.com>
2025-02-14 14:18:23 +0530

    Inheritance

---
+++ inheritance/TestInheritance.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class TestInheritance {
+    public static void main(String[] args) {
+        Student s1=new Student("Amruta",1234567890,"Kolhapur",61,"Keystone");
+        System.out.println(s1);
+    }
+}
